25790f86236b4a051dda785fd3261f265c1aa9ec
Pedro-Taiette <pedrolibrais02@gmail.com>
2025-05-28 16:59:32 -0300

    Add ViewNotes and ViewAllNotes to urls and views of Subjects

--- AllUni/subjects/urls.py
+++ AllUni/subjects/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.subject_list, name='subject_list'),
     path('add/', views.add_subject, name='add_subject'),
+    path('notes/', views.all_notes, name='all_notes'), 
+    path('notes/<int:note_id>/', views.note_detail, name='note_detail'), 
     path('<int:subject_id>/', views.subject_detail, name='subject_detail'),
     path('<int:subject_id>/add_note/', views.add_note, name='add_note'),
 ]
