7401ef78affc3c71c6bb4321ec900eb64b7463fe
Pedro-Taiette <pedrolibrais02@gmail.com>
2025-05-28 22:02:12 -0300

    Add Search to Dashboard

--- AllUni/subjects/urls.py
+++ AllUni/subjects/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('add/', views.add_subject, name='add_subject'),
     path('notes/', views.all_notes, name='all_notes'), 
     path('notes/<int:note_id>/', views.note_detail, name='note_detail'), 
+    path('search/', views.search, name='search'),
     path('<int:subject_id>/', views.subject_detail, name='subject_detail'),
     path('<int:subject_id>/add_note/', views.add_note, name='add_note'),
 ]
